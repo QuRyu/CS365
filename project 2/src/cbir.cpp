73d3f9193d4efcb8f3d207fd690063fa03d4f2d6
QuRyu <Quentin.Liu.0415@gmail.com>
2019-02-24 17:26:12 -0500

    Traverse and read images

--- project 2/src/cbir.cpp
+++ project 2/src/cbir.cpp
@@ -6,10 +6,74 @@
  *
  */
 #include <cstdio> 
+#include <cstdlib>
+#include <dirent.h>
+#include <cstring> 
+#include <string> 
+#include <map> 
+
 #include <opencv2/opencv.hpp> 
 
+const int QUERY_IMAGE_FP = 1; 
+const int DATABASE_FP = 2; 
+const int METRIC = 3; 
+const int N = 4; 
+
+std::vector<std::string> 
+traverse_dir(char *dir_fp) { 
+    std::vector<std::string> images_fp; 
+    DIR *dirp; 
+    struct dirent *dp; 
+        
+    dirp = opendir(dir_fp);
+
+    if (dirp == NULL) {
+        printf("Cannot open directory %s\n", dir_fp);
+        exit(-1);
+    }
+
+    while ((dp = readdir(dirp)) != NULL) {
+        if (strstr(dp->d_name, ".jpg") ||
+                strstr(dp->d_name, ".png") || 
+                strstr(dp->d_name, ".arw") || 
+                strstr(dp->d_name, ".ppm") || 
+                strstr(dp->d_name, ".tif")) {
+            images_fp.push_back(dp->d_name);
+        }
+    }
+
+    closedir(dirp); 
+
+    return images_fp;
+}
+
+/**
+ * Given the file path, return the image 
+ */
+cv::Mat read_image(std::string fp) {
+    return cv::imread(fp);
+}
+
+std::map<std::string, double> 
+compare(cv::Mat query, const std::vector<std::string> &database, 
+        std::function<int(const cv::Mat,const cv::Mat)> func) { 
+
+
+}
+
 int main(int argc, char *argv[]) { 
-    std::cout << "hello" << std::endl;
+    // command line argument format 
+    // 1: query image file path 
+    // 2: database file path 
+    // 3: which metrics to use 
+    // 4: N -- number of results to show 
+
+    if (argc != 5) 
+        std::cerr << "expects four arguments" << std::endl;
+
+    auto images = traverse_dir(argv[DATABASE_FP]);
+
+
     return 0; 
 }
 
