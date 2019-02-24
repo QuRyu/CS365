9556c7556793e5b96d3bc4610ec3a85aab3fca6b
QuRyu <Quentin.Liu.0415@gmail.com>
2019-02-24 16:21:23 -0500

    skeleton for project 2

---
+++ project 2/src/cbir.cpp
@@ -0,0 +1,16 @@
+/*
+ *
+ * CS365 19 Spring 
+ *
+ * Assignment 2: Content-based image retrieval 
+ *
+ */
+#include <cstdio> 
+#include <opencv2/opencv.hpp> 
+
+int main(int argc, char *argv[]) { 
+    std::cout << "hello" << std::endl;
+    return 0; 
+}
+
+
