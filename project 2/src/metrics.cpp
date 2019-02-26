742bd44457967d9a3fb58bfc770dc1b90ae55994
irislian <ylian@fili.cs.colby.edu>
2019-02-25 19:58:13 -0500

    added metrics.cpp

---
+++ project 2/src/metrics.cpp
@@ -0,0 +1,31 @@
+/*
+ * metrics.cpp
+ *
+ * CS365 19 Spring 
+ *
+ * Assignment 2: Content-based image retrieval 
+ *
+ */
+#include <cstdio> 
+#include <cstdlib>
+#include <dirent.h>
+#include <cstring> 
+#include <string> 
+#include <map> 
+
+#include <opencv2/opencv.hpp> 
+
+// Baseline Matching
+double ssd_metric(const cv::Mat query,const cv::Mat img){
+    double distance = 0.0;
+
+    return distance;
+}
+
+// Baseline Histogram Matching
+
+// Multiple Histogram Matching
+
+// Integrating Texture and Color
+
+// Custom Distance Metric
