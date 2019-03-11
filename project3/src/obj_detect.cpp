79308798695f4e978bf8b671e9172cfa266aabee
QuRyu <Quentin.Liu.0415@gmail.com>
2019-03-10 23:39:31 -0400

    Moved image operators to imgproc

--- project3/src/obj_detect.cpp
+++ project3/src/obj_detect.cpp
@@ -10,6 +10,7 @@
 
 #include <opencv2/opencv.hpp>
 
+#include "imgproc.hpp"
 #include "utilities.hpp"
 
 using namespace std;
@@ -20,15 +21,6 @@ static const unsigned char FOREGROUND = 0;
 
 using elem_type = unsigned char; 
 
-Mat threshold(const Mat &src) {
-    Mat grayscale, dst; 
-
-
-    cv::cvtColor(src, grayscale, COLOR_BGR2GRAY);
-    cv::threshold(grayscale, dst, 120, 255, THRESH_BINARY);
-
-    return dst;
-}
 
 pair<int, Mat> comp_seg(const Mat &src) { 
     Mat regmap; 
@@ -38,86 +30,6 @@ pair<int, Mat> comp_seg(const Mat &src) {
     return make_pair(label, regmap);
 }
 
-// assume the type is 8UC1 -- unsigned char 
-// use 4-connected ways 
-Mat morph_shrink(const Mat &src) {
-    Mat shrunk; 
-    int sum; 
-
-    shrunk.create(src.rows, src.cols, CV_8UC1);
-
-    for (int i=0; i<src.rows; i++) {
-	for (int j=0; j<src.cols; j++) {
-	    auto A1 = i == src.rows-1 ? BACKGROUND : src.at<elem_type>(i+1, j); 
-	    auto A3 = j == 0 ? BACKGROUND : src.at<elem_type>(i, j-1);
-	    auto A5 = i == 0 ? BACKGROUND : src.at<elem_type>(i-1, j);
-	    auto A7 = j == src.cols-1 ? BACKGROUND : src.at<elem_type>(i, j+1);
-
-	    // initialize sum and add neighbors 
-	    // (convert the values to avoid overflow)
-	    sum = A1; 
-	    sum += A3; 
-	    sum += A5;
-	    sum += A7;
-
-	    if (sum > 0) 
-		shrunk.at<elem_type>(i, j) = BACKGROUND; 
-	    else 
-		shrunk.at<elem_type>(i, j) = src.at<elem_type>(i, j);
-	}
-    }
-
-    return shrunk;
-}
-
-Mat morph_dilate(const Mat &src) { 
-    Mat dilated; 
-    int sum; 
-
-    dilated.create(src.rows, src.cols, CV_8UC1);
-
-    for(int i=0; i<src.rows; i++) {
-	for(int j=0; j<src.cols; j++) {
-	    // find adjacent neighbors 
-	    auto A1 = i == src.rows-1 ? BACKGROUND : src.at<elem_type>(i+1, j); 
-	    auto A3 = j == 0 ? BACKGROUND : src.at<elem_type>(i, j-1);
-	    auto A5 = i == 0 ? BACKGROUND : src.at<elem_type>(i-1, j);
-	    auto A7 = j == src.cols-1 ? BACKGROUND : src.at<elem_type>(i, j+1);
-
-	    // initialize sum and add neighbors 
-	    // (convert the values to avoid overflow)
-	    sum = A1; 
-	    sum += A3; 
-	    sum += A5;
-	    sum += A7;
-
-	    if (sum < BACKGROUND*4) 
-		dilated.at<elem_type>(i, j) = FOREGROUND;
-	    else 
-		dilated.at<elem_type>(i, j) = src.at<elem_type>(i, j);
-	}
-    }
-
-    return dilated; 
-}
-
-// opening: shrink and then grow 
-// eliminates noise 
-Mat morph_opening(const Mat &src) { 
-    auto shrunk = morph_shrink(src);
-    auto dilated = morph_dilate(shrunk);
-
-    return dilated;
-}
-
-// closing: grow and then shrink 
-// closes holes in target objects 
-Mat morph_closing(const Mat &src) {
-    auto dilated = morph_dilate(src);
-    auto shrunk = morph_shrink(dilated);
-
-    return dilated;
-}
 
 
 Mat process_img(const Mat &src) {
